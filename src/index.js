4bf0c734fed4b84f8d78ab6a869f00d631e74d13
riversun <riversun.org@gmail.com>
2019-09-22 13:04:14 +0900

    add js to handle events from bootstrap components

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-import "bootstrap";
-import "./index.scss";
+import 'bootstrap';
+import './index.scss';
+
+$('button[id^=bt]').on('click', e => {
+    alert(`${$(e.target).attr('id')} "${$(e.target).text()}" clicked`);
+});
+
+$('.list-group-item').on('click', e => {
+    alert(`"${$(e.target).text()}" clicked`);
+});
+
+$('#flip-tab').on('click', e => {
+    $('.nav-tabs :not(.active)').tab('show')
+});
+
+$('a[data-toggle="tab"]').on('click', e => {
+    alert(`${$(e.target).attr('id')} clicked`);
+});
